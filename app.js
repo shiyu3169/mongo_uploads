2e1bf395f71047f223437f88b456fd4cf655297b
Shiyu Wang <shiyu3169@gmail.com>
2019-08-06 11:59:24 -0400

    delete image

--- app.js
+++ app.js
@@ -124,6 +124,17 @@ app.get("/image/:filename", (req, res) => {
   });
 });
 
+// route DELETE /files/:id
+// desc Delete file
+app.delete("/files/:id", (req, res) => {
+  gfs.remove({ _id: req.params.id, root: "uploads" }, (err, gridStore) => {
+    if (err) {
+      return res.status(404).json({ err: err });
+    }
+    res.redirect("/");
+  });
+});
+
 const port = 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
